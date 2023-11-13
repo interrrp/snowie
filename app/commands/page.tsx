dac989e5c26f9583a8fd737d2db362375b8c696a
int <watchyourcache@gmail.com>
2023-11-13 18:01:51 +0800

    Add back button to commands page

--- app/commands/page.tsx
+++ app/commands/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import {
   Table,
   TableBody,
@@ -7,31 +9,44 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { COMMANDS } from "@/lib/commands";
+import { ArrowLeft } from "lucide-react";
 
 export default function Commands() {
   return (
-    <main className="md:w-3/4 mx-auto md:p-16 p-12">
-      <h1 className="text-2xl font-bold mb-4">Commands</h1>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Command</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Example</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {COMMANDS.map((command) => (
-            <TableRow key={command.name}>
-              <TableCell>{command.name}</TableCell>
-              <TableCell>{command.description}</TableCell>
-              <TableCell>
-                <code>{`/${command.name} ${command.example || ""}`}</code>
-              </TableCell>
+    <div className="md:w-3/4 mx-auto md:p-16 p-12">
+      <header>
+        <nav>
+          <h1 className="text-2xl font-bold mb-4">
+            <Link href="/" aria-label="Go back">
+              <ArrowLeft className="inline mr-2 -translate-y-0.5" />
+            </Link>{" "}
+            Commands
+          </h1>
+        </nav>
+      </header>
+
+      <main>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Command</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Example</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </main>
+          </TableHeader>
+          <TableBody>
+            {COMMANDS.map((command) => (
+              <TableRow key={command.name}>
+                <TableCell>{command.name}</TableCell>
+                <TableCell>{command.description}</TableCell>
+                <TableCell>
+                  <code>{`/${command.name} ${command.example || ""}`}</code>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
   );
 }
